b3f5eeed09fe904d53f3f815e8ef6e0256601f23
Jake Daylongd <jakedaylong@outlook.com>
2023-01-04 11:14:46 -0800

    updated chart naming

--- operation script/bug_charts.py
+++ operation script/bug_charts.py
@@ -9,7 +9,12 @@ def count_of_bugs_creation_date(bug_df):
     palette = {'Blocker':'tab:red', 'Major':'tab:orange', 'Minor':'tab:green', 'Trivial':'tab:blue'}
     sns.set_context('notebook')
     sns.histplot(data=bug_df, x='Created', hue='Priority', multiple='stack', palette=palette)
-    plt.savefig(fname='count_of_bugs_creation_date')
+    plt.savefig(fname='Count_of_Bugs_Creation_Date')
+    
+    html_str = mpld3.fig_to_html(fig)
+    html_file= open("Count_by_Priority.html","w")
+    html_file.write(html_str)
+    html_file.close()
 
 def count_by_priority(bug_df):
     '''count of total bugs by priority. (includes resolved bugs)'''
@@ -18,12 +23,12 @@ def count_by_priority(bug_df):
     palette = {'Customer Action':'tab:red','In Progress':'tab:blue', 'Open':'tab:orange', 'Resolved':'tab:green', 'in Discussion':'tab:grey'}
     sns.set_context('notebook')
     sns.histplot(data=bug_df, x='Priority', hue='Status', multiple='stack', palette=palette)
-    plt.savefig(fname='Count_By_Priority')
+    plt.savefig(fname='Count_by_Priority')
 
     html_str = mpld3.fig_to_html(fig)
-    Html_file= open("count_by_priority.html","w")
-    Html_file.write(html_str)
-    Html_file.close()
+    html_file= open("Count_by_Priority.html","w")
+    html_file.write(html_str)
+    html_file.close()
 
 def count_by_status(bug_df):
     '''count of bugs by current status. colored by priotity. (includes resolved bugs)'''
@@ -32,12 +37,12 @@ def count_by_status(bug_df):
     palette = {'Blocker':'tab:red', 'Major':'tab:orange', 'Minor':'tab:green', 'Trivial':'tab:blue'}
     sns.set_context('notebook')
     sns.histplot(data=bug_df, x='Status', hue='Priority', multiple='stack', palette=palette)
-    plt.savefig(fname='Count_By_Status')
+    plt.savefig(fname='Count_by_Status')
     
     html_str = mpld3.fig_to_html(fig)
-    Html_file= open("count_by_status.html","w")
-    Html_file.write(html_str)
-    Html_file.close()
+    html_file= open("Count_by_Status.html","w")
+    html_file.write(html_str)
+    html_file.close()
 
 def cumsum_total_res(bug_df_cumsum, bug_df_cumsum_res):
     fig, ax = plt.subplots(figsize=(12,8))
@@ -47,15 +52,15 @@ def cumsum_total_res(bug_df_cumsum, bug_df_cumsum_res):
     plt.savefig(fname='Cumsum_Total_vs_Resolved')
 
     html_str = mpld3.fig_to_html(fig)
-    Html_file= open("Sum_total_vs_resolved.html","w")
-    Html_file.write(html_str)
-    Html_file.close()
+    html_file= open("Sum_Total_vs_Resolved.html","w")
+    html_file.write(html_str)
+    html_file.close()
 
 def count_of_bugs_resolved(bug_df):
     '''Creates histplot for count of bugs by creations date from bug_df dataframe
-    and colored by bug priority. (includes resolved bugs)'''
+    and colored by bug status. (includes resolved bugs)'''
     fig, ax = plt.subplots(figsize=(12,8))
     palette = {'Customer Action':'tab:red', 'In Progress':'tab:orange', 'Resolved':'tab:green', 'in Discussion':'tab:blue'}
     sns.set_context('notebook')
     sns.histplot(data=bug_df, x='Created', hue='Status', multiple='stack')
-    plt.savefig(fname='count_of_bugs_resolved')
+    plt.savefig(fname='Count_of_Bugs_Resolved')
